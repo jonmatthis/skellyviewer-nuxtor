633890b0bd911dc150bc39be0ff531d71056cc47
jonmatthis <jonmatthis@gmail.com>
2024-07-29 09:16:21 -0400

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,68 @@
+export default defineNuxtConfig({
+	modules: [
+		"@vueuse/nuxt",
+		"@unocss/nuxt",
+		"nuxt-svgo",
+		"@nuxt/eslint"
+	],
+	app: {
+		head: {
+			title: "Nuxtor",
+			charset: "utf-8",
+			viewport: "width=device-width, initial-scale=1",
+			meta: [
+				{ name: "format-detection", content: "no" }
+			],
+			bodyAttrs: {
+				class: "font-text antialiased"
+			}
+		},
+		pageTransition: {
+			name: "page",
+			mode: "out-in"
+		},
+		layoutTransition: {
+			name: "layout",
+			mode: "out-in"
+		}
+	},
+	experimental: {
+		typedPages: true
+	},
+	css: [
+		"@unocss/reset/tailwind.css"
+	],
+	svgo: {
+		autoImportPath: "@/assets/"
+	},
+	vite: {
+		clearScreen: false,
+		envPrefix: ["VITE_", "TAURI_"],
+		server: {
+			strictPort: true,
+			hmr: {
+				protocol: "ws",
+				host: "0.0.0.0",
+				port: 3001
+			},
+			watch: {
+				ignored: ["**/src-tauri/**"]
+			}
+		}
+	},
+	vue: {
+		compilerOptions: {
+			isCustomElement: (tag: string) => tag.startsWith("i-")
+		}
+	},
+	srcDir: "src/",
+	ssr: false,
+	devServer: {
+		host: "0.0.0.0"
+	},
+	eslint: {
+		config: {
+			standalone: false
+		}
+	}
+});
---
+++ uno.config.ts
@@ -0,0 +1,55 @@
+import {
+	defineConfig,
+	presetAttributify,
+	presetIcons,
+	presetTagify,
+	presetTypography,
+	presetUno,
+	presetWebFonts,
+	transformerDirectives,
+	transformerVariantGroup
+} from "unocss";
+
+export default defineConfig({
+	shortcuts: [
+		["crate", "mx-auto max-w-7xl px-4 lg:px-8 sm:px-6"],
+		["flex-center", "flex justify-center items-center"],
+		["absolute-center-h", "left-50% transform -translate-x-50%"],
+		["absolute-center-v", "top-50% transform -translate-y-50%"]
+	],
+	presets: [
+		presetUno(),
+		presetTagify(),
+		presetAttributify(),
+		presetIcons({
+			extraProperties: {
+				display: "inline-block",
+				"vertical-align": "middle"
+			}
+		}),
+		presetTypography(),
+		presetWebFonts({
+			fonts: {
+				heading: {
+					name: "Montserrat",
+					weights: ["200", "400", "700"]
+				},
+				text: "Inter"
+			}
+		})
+	],
+	theme: {
+		breakpoints: {
+			sm: "576px",
+			md: "768px",
+			lg: "992px",
+			xl: "1200px",
+			xxl: "1400px",
+			uw: "2000px"
+		}
+	},
+	transformers: [
+		transformerDirectives(),
+		transformerVariantGroup()
+	]
+});

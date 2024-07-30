59bddde95b7a4c37cada7e742e67fbe68cd0466c
jonmatthis <jonmatthis@gmail.com>
2024-07-30 09:31:37 -0400

    `csv/` endpoint returns test data csv

--- src-python/skellyviewer-python/__main__.py
+++ src-python/skellyviewer-python/__main__.py
@@ -1,18 +1,44 @@
-from typing import Union
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, WebSocket
-from pydantic import BaseModel
+import pandas as pd
+from pathlib import Path
 
-app = FastAPI()
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+FREEMOCAP_TEST_DATA_CSV_PATH = r"C:\Users\jonma\freemocap_data\recording_sessions\freemocap_test_data\freemocap_test_data_by_trajectory.csv"
+
+@asynccontextmanager
+async def lifespan_manager(app: FastAPI):
+    logger.info("Starting up FastAPI app - access API backend interface at http://localhost:8000/docs")
+    yield
+    logger.info("Shutting down FastAPI app")
+
+app = FastAPI(lifespan=lifespan_manager)
 
 
 @app.get("/")
 def read_root():
-    return {"Wowwee": "Zowee"}
+    logger.info("Received request for route /")
+    return {"Wowwee": "Zoweeeeee"}
+
+@app.get("/csv")
+def read_csv(csvPath: str = FREEMOCAP_TEST_DATA_CSV_PATH):
+    logger.info(f"Received request for route /csv with csvPath: {csvPath}")
+
+    if not Path(csvPath).exists():
+        logger.error(f"File not found: {csvPath}")
+        return {"error": f"File not found: {csvPath}"}
+    df = pd.read_csv(csvPath)
+    df_json = df.to_json()
+    logger.info(f"Read csv file with shape: {df.shape}")
+    return df_json
 
 
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
@@ -21,4 +47,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app,
+     host="localhost",
+     port=8000)
